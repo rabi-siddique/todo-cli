72d9236b2cf64fa23ff35d57bf4cab9b695256b8
rabi-siddique <rabisiddique27@gmail.com>
2024-10-16 22:23:02 +0500

    feat: remove item

--- todo.lua
+++ todo.lua
@@ -1,9 +1,31 @@
-file = io.open("todos", "a")
-
 function addItem(item)
+    local file = io.open("todos", "a")
     local itemToAdd = string.format("%s\n", item)
     file:write(itemToAdd)
     file:close()
 end
 
-addItem("HELLO")
+function removeItem(item)
+    local file = io.open("todos", "r")
+    local lines = file:lines()
+    local linesTable = {}
+
+    for line in lines do
+        if line ~= item then
+            table.insert(linesTable, line)
+        end
+    end
+
+    file:close()
+
+    file = io.open("todos", "w")
+
+    for _, line in ipairs(linesTable) do
+        local itemToAdd = string.format("%s\n", line)
+        file:write(itemToAdd)
+    end
+
+    file:close()
+
+end
+
