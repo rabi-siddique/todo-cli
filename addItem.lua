351a8385177d1952e8adc9ba4f5057938735e07a
rabi-siddique <rabisiddique27@gmail.com>
2024-10-19 10:24:30 +0500

    refactor: turn addItem into its own module

---
+++ addItem.lua
@@ -0,0 +1,23 @@
+local utils = require("utils")
+local add = {}
+function add.addItem(filePath, item)
+    local file, error = io.open(filePath, "a")
+
+    if error then
+        print("ERROR:", error)
+        return
+    end
+
+    local itemID = tostring(os.time())
+    local itemToAdd = string.format("%s,%s,%s\n", itemID, item, category)
+    file:write(itemToAdd)
+    file:close()
+
+    if verbose then
+        print("Todo Category:" .. category)
+    end
+
+    print("Todo Added Successfully!")
+end
+
+return add
